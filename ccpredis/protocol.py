56050049b305d7e2956b5a75fa9887301811f6c7
lurumad <luisruizpavon@gmail.com>
2023-11-19 11:55:09 +0100

    feat: simple string incomplete

--- ccpredis/protocol.py
+++ ccpredis/protocol.py
@@ -1,4 +1,10 @@
+RESP_TERMINATOR = '\r\n'
+
 def parse(buffer: str) -> (int, str):
     match buffer[0]:
         case '+':
-            return buffer[1:-2], len(buffer)
+            terminator = buffer.find(RESP_TERMINATOR)
+
+            if terminator != -1:
+                return buffer[1:-2], len(buffer)
+    return (None, 0)
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -6,3 +6,9 @@ def test_simple_string():
     simple_string, size = protocol.parse(buffer)
     assert "OK" == simple_string
     assert 5 == size
+
+def test_simple_string_incomplete():
+    buffer = "+OK"
+    simple_string, size = protocol.parse(buffer)
+    assert None == simple_string
+    assert 0 == size
