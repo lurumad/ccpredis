7912b635ccc97fbcf006b480e4a7cefbc5e12b01
lurumad <luisruizpavon@gmail.com>
2023-11-19 16:42:31 +0100

    refactor: guard clause

--- ccpredis/protocol.py
+++ ccpredis/protocol.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
-RESP_TERMINATOR = '\r\n'
+PROTOCOL_TERMINATOR = '\r\n'
+PROTOCOL_TERMINATOR_LEN = len(PROTOCOL_TERMINATOR)
 
 
 @dataclass
@@ -19,20 +20,17 @@ class Integer:
 
 
 def parse(buffer: str) -> (int, str):
+    protocol_terminator = buffer.find(PROTOCOL_TERMINATOR)
+    if protocol_terminator == -1:
+        return None, 0
+
     match buffer[0]:
         case '+':
-            resp_terminator = buffer.find(RESP_TERMINATOR)
-            if resp_terminator != -1:
-                simple_string = SimpleString(buffer[1:resp_terminator])
-                return simple_string, 1 + len(simple_string.value + RESP_TERMINATOR)
+            simple_string = SimpleString(buffer[1:protocol_terminator])
+            return simple_string, 1 + len(simple_string.value) + PROTOCOL_TERMINATOR_LEN
         case '-':
-            resp_terminator = buffer.find(RESP_TERMINATOR)
-            if resp_terminator != -1:
-                simple_error = SimpleError(buffer[1:resp_terminator])
-                return simple_error, 1 + len(simple_error.value + RESP_TERMINATOR)
+            simple_error = SimpleError(buffer[1:protocol_terminator])
+            return simple_error, 1 + len(simple_error.value) + PROTOCOL_TERMINATOR_LEN
         case ':':
-            resp_terminator = buffer.find(RESP_TERMINATOR)
-            if resp_terminator != -1:
-                integer = Integer(int(buffer[1:resp_terminator]))
-                return integer, 0
-    return None, 0
+            integer = Integer(int(buffer[1:protocol_terminator]))
+            return integer, 0
