4e2f2542583757e552397da01e1d414a30664cd7
lurumad <luisruizpavon@gmail.com>
2023-11-19 16:26:12 +0100

    feat: simple error

--- ccpredis/protocol.py
+++ ccpredis/protocol.py
@@ -1,7 +1,17 @@
-from ccpredis.data_types.simple_string import SimpleString
+from dataclasses import dataclass
 
 
 RESP_TERMINATOR = '\r\n'
+
+
+@dataclass
+class SimpleString:
+    value: str
+
+@dataclass
+class SimpleError:
+    value: str
+
 
 def parse(buffer: str) -> (int, str):
     match buffer[0]:
@@ -10,4 +20,9 @@ def parse(buffer: str) -> (int, str):
             if resp_terminator != -1:
                 simple_string = SimpleString(buffer[1:resp_terminator])
                 return simple_string, 1 + len(simple_string.value + RESP_TERMINATOR)
-    return (None, 0)
+        case '-':
+            resp_terminator = buffer.find(RESP_TERMINATOR)
+            if resp_terminator != -1:
+                simple_error = SimpleError(buffer[1:resp_terminator])
+                return simple_error, 1 + len(simple_error.value + RESP_TERMINATOR)
+    return None, 0
--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,6 +1,7 @@
 import pytest
 from ccpredis import protocol
-from ccpredis.data_types.simple_string import SimpleString
+from ccpredis.protocol import SimpleString, SimpleError
+
 
 @pytest.mark.parametrize("buffer, expected", [
     ("+OK\r\n", (SimpleString("OK"), 5)),
@@ -10,12 +11,20 @@ def test_simple_string(buffer, expected):
     actual = protocol.parse(buffer)
     assert actual == expected
 
+
 def test_simple_string_incomplete():
     buffer = "+OK"
     actual = protocol.parse(buffer)
     assert actual == (None, 0)
 
+
 def test_simple_string_extra_data():
     buffer = "+OK\r\nextra"
     actual = protocol.parse(buffer)
     assert actual == (SimpleString("OK"), 5)
+
+
+def test_simple_error():
+    buffer = "-Error message\r\n"
+    actual = protocol.parse(buffer)
+    assert actual == (SimpleError("Error message"), 16)
