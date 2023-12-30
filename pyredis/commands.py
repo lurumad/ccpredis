e3837c3cff31c687e7a4a25520116aefe3fd72e7
lurumad <luisruizpavon@gmail.com>
2023-12-30 08:45:59 +0100

    refactor: extract handle commands

--- pyredis/commands.py
+++ pyredis/commands.py
@@ -6,60 +6,76 @@ def handle_command(command: Array, datastore: DataStore):
     command, *command_args = command.data
     match command.data.decode().upper():
         case "PING":
-            if len(command_args) == 0:
-                return SimpleString("PONG")
-            if len(command_args) == 1:
-                return BulkString(command_args[0].data)
-            return Error("ERR wrong number of arguments for 'ping' command")
+            return handle_ping(command_args)
         case "ECHO":
-            if len(command_args) == 1:
-                return BulkString(command_args[0].data)
-            return Error("ERR wrong number of arguments for 'echo' command")
+            return handle_echo(command_args)
         case "SET":
-            if len(command_args) < 2:
-                return Error("ERR wrong number of arguments for 'set' command")
-
-            key = command_args[0].data.decode()
-            value = command_args[1].data.decode()
-
-            if len(command_args) == 2:
-                datastore[key] = value
-                return SimpleString("OK")
-
-            if len(command_args) == 4:
-                option = command_args[2].data.decode()
-                try:
-                    option_value = float(command_args[3].data.decode())
-                except ValueError:
-                    return Error("ERR value is not an integer or out of range")
-
-                match option.upper():
-                    case "EX":
-                        datastore.set_with_expiry(
-                            key=key, value=value, expiry=option_value
-                        )
-                        return SimpleString("OK")
-                    case "PX":
-                        datastore.set_with_expiry(
-                            key=key, value=value, expiry=option_value / 1000
-                        )
-                        return SimpleString("OK")
-
-            return Error("ERR syntax error")
+            return handle_set(command_args, datastore)
         case "GET":
-            if len(command_args) != 1:
-                return Error("ERR wrong number of arguments for 'get' command")
-            try:
-                key = command_args[0].data.decode()
-                value = datastore[key]
-            except KeyError:
-                return BulkString(None)
-            return BulkString(value.encode())
+            return handle_get(command_args, datastore)
 
+    return handle_unknown(command, command_args)
+
+
+def handle_unknown(command, command_args):
     args = " ".join([f"'{arg.data.decode()}'" for arg in command_args])
     return Error(
         f"ERR unknown command '{command.data.decode().upper()}', with args beginning with: {args}"
     )
+
+
+def handle_get(command_args, datastore):
+    if len(command_args) != 1:
+        return Error("ERR wrong number of arguments for 'get' command")
+    try:
+        key = command_args[0].data.decode()
+        value = datastore[key]
+    except KeyError:
+        return BulkString(None)
+    return BulkString(value.encode())
+
+
+def handle_set(command_args, datastore):
+    if len(command_args) < 2:
+        return Error("ERR wrong number of arguments for 'set' command")
+    key = command_args[0].data.decode()
+    value = command_args[1].data.decode()
+    if len(command_args) == 2:
+        datastore[key] = value
+        return SimpleString("OK")
+    if len(command_args) == 4:
+        option = command_args[2].data.decode()
+        try:
+            option_value = float(command_args[3].data.decode())
+        except ValueError:
+            return Error("ERR value is not an integer or out of range")
+
+        match option.upper():
+            case "EX":
+                datastore.set_with_expiry(
+                    key=key, value=value, expiry=option_value
+                )
+                return SimpleString("OK")
+            case "PX":
+                datastore.set_with_expiry(
+                    key=key, value=value, expiry=option_value / 1000
+                )
+                return SimpleString("OK")
+    return Error("ERR syntax error")
+
+
+def handle_echo(command_args):
+    if len(command_args) == 1:
+        return BulkString(command_args[0].data)
+    return Error("ERR wrong number of arguments for 'echo' command")
+
+
+def handle_ping(command_args):
+    if len(command_args) == 0:
+        return SimpleString("PONG")
+    if len(command_args) == 1:
+        return BulkString(command_args[0].data)
+    return Error("ERR wrong number of arguments for 'ping' command")
 
 
 def encode_command(command):
